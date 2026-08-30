1487c88e25713e0d1c7a320eaf62983e197fdf43
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-08-30 17:04:19 +0500

    task1 updated

--- labtask2/task1.cpp
+++ labtask2/task1.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 using namespace std;
 
-void print(){
-    cout<<"yay"<<endl;
+void Fillarray(int **ptr, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> ptr[i][j];
+        }
+    }
 }
 
-int main() {
-    print();
-    
+int findlargest(int **matrix, int rows, int columns)
+{
+}
+
+int main()
+{
+    int row, col;
+
+    cout << "enter rows : ";
+    cin >> row;
+
+    cout << "enter columns : ";
+    cin >> col;
+
+    int **ptr = new int *[row];
+    for (int i = 0; i < row; i++)
+    {
+        ptr[i] = new int[col];
+    }
+
     return 0;
 }
