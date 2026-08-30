b677e6b1b713999ebff0b2e34b89d6d0c69611c0
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-08-31 00:19:17 +0500

    new task3

---
+++ labtask2/task3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int countNegatives(int **grid, int rows, int columns)
+{
+    int neg = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (grid[i][j] < 0)
+            {
+                neg++;
+            }
+        }
+    }
+    return neg;
+}
+
+void Print(int neg){
+    cout<<neg<<endl;
+}
+
+int main()
+{
+    int row, col;
+
+    cout << "enter rows : ";
+    cin >> row;
+
+    cout << "enter columns : ";
+    cin >> col;
+
+    int **ptr = new int *[row];
+    for (int i = 0; i < row; i++)
+    {
+        ptr[i] = new int[col];
+    }
+    int negativeNums = countNegatives(ptr, row, col);
+    Print(negativeNums);
+
+    for (int i = 0; i < row; i++)
+    {
+        delete[] ptr[i];
+    }
+    delete[] ptr;
+    
+    return 0;
+}
