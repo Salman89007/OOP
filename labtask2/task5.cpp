abaef409b5607a5edb8d0fe90a426168e3e750a7
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-08-31 18:55:39 +0500

    changes to task5

--- labtask2/task5.cpp
+++ labtask2/task5.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
+void Fillarray(int **ptr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cin >> ptr[i][j];
+        }
+    }
+}
+
+void Print(int **ptr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << ptr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    int size;
+
+    cout << "enter size of X grid : ";
+    cin >> size;
+
+    int **ptr = new int *[size];
+    for (int i = 0; i < size; i++)
+    {
+        ptr[i] = new int[size];
+    }
+
+    Fillarray(ptr, size);
+    Print(ptr, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        delete[] ptr[i];
+    }
+    delete[] ptr;
+
     return 0;
 }
