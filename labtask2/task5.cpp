cf04339dafc307ea3578aaec8cf95348f97e4b0d
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-08-31 19:38:43 +0500

    changes to final task of labtask2

--- labtask2/task5.cpp
+++ labtask2/task5.cpp
@@ -12,6 +12,46 @@ void Fillarray(int **ptr, int size)
     }
 }
 
+int Count(int **ptr, int size)
+{
+    int num = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (ptr[i][j] == 1)
+            {
+                num++;
+            }
+        }
+    }
+    return num;
+}
+
+void Found(int **ptr, int size)
+{
+    int row, col;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << "enter rows : ";
+            cin >> row;
+
+            cout << "enter columns : ";
+            cin >> col;
+            if (ptr[i][j] == ptr[row][col])
+            {
+                ptr[i][j] = 0;
+            }
+        }
+    }
+}
+void printCount(int count){
+    cout<<"enemies alive : "<<count<<endl;
+}
+
 void Print(int **ptr, int size)
 {
     for (int i = 0; i < size; i++)
@@ -39,6 +79,9 @@ int main()
     }
 
     Fillarray(ptr, size);
+    int count = Count(ptr, size);
+    printCount(count);
+    Found(ptr, size);
     Print(ptr, size);
 
     for (int i = 0; i < size; i++)
