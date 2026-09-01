78f7c79d865a2e9507dac80dafd78e77ce154c65
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-01 23:17:52 +0500

    little code added into pure virtual

--- abstract_pureVirtual.cpp
+++ abstract_pureVirtual.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class base{
+    protected:
+    int x;int y;
+    public:
+    base(int x, int y){
+        this->x = x;
+        this->y = y;
+    }
+    virtual void showFunc() = 0;
+
+};
+class derived : public base{
+    protected:
+    int x,y;
+    int z;
+    public:
+    derived(int x,int y,int z):base(x,y){
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+};
+
 int main() {
 
-    
+
     
     return 0;
 }
