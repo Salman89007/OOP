8338db38bda4c71f57ff9ad5664cf752a8665de9
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-01 19:19:16 +0500

    Operator overloding complete

--- Operator_overloading.cpp
+++ Operator_overloading.cpp
@@ -3,34 +3,30 @@ using namespace std;
 
 class base
 {
-    int sum1;
-    int sum2;
     int x;
     int y;
 
 public:
     base()
     {
-        sum1 = 0;
-        sum2 = 0;
     }
-
     base(int x, int y)
     {
         this->x = x;
         this->y = y;
     }
-    base add(base &b)
-    {
+    base operator+(base &b){
         base result;
         result.x = b.x + x;
         result.y = b.y + y;
         return result;
+
     }
+    
     void print()
     {
         cout << "x : " << x << endl;
-        cout << "y : " << y << endl;
+        cout << "y : " << y << endl;   
     }
 };
 
@@ -38,8 +34,7 @@ int main()
 {
     base b1(1, 2);
     base b2(4, 3);
-
-    base result = b1.add(b2);
+    base result = b1+b2;
     result.print();
 
     return 0;
