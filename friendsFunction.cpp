af88b74677500739677ee4dbd1ef7666aeea0585
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-01 23:58:00 +0500

    friends second commit

--- friendsFunction.cpp
+++ friendsFunction.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class Friend{
+    int x;
+    public:
+    Friend(int X){
+        x = X;
+    }
+    friend void showX(Friend f);
+
+};
+void showX(Friend f){
+    cout<<f.x<<endl;
+}
+
 int main() {
+    Friend f(5);
+    showX(f);
     
     return 0;
 }
