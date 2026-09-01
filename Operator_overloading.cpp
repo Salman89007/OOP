975c92c9cf85c5fcce8839abd2aeb95ad37e83be
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-01 18:26:17 +0500

    operator overloading not done yet

--- Operator_overloading.cpp
+++ Operator_overloading.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class base
+{
+protected:
+    int sum1 = 0;
+    int sum2 = 0;
+    int x;
+    int y;
 
-    
-    
+public:
+    base(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    int add(base &b)
+    {
+        return sum1 = b.x + x;
+        return sum2 = b.y + y;
+    }
+    void print(const base &b)
+    {
+        cout << sum1;
+        cout << sum2;
+    }
+};
+
+class derived : public base
+{
+};
+
+int main()
+{
+    base b(1, 2);
+    b.add(b);
+    b.print(b);
+
     return 0;
 }
