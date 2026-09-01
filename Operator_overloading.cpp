d3b097c2d9921c4616d92996b4b67c370924dee4
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-01 19:50:07 +0500

    Operator overloding complete

--- Operator_overloading.cpp
+++ Operator_overloading.cpp
@@ -10,19 +10,19 @@ public:
     base()
     {
     }
-    base(int x, int y)
-    {
-        this->x = x;
-        this->y = y;
+    base(int x, int y)//for b1 1 2 gets stored
+    {                 //for b2 4 and 3 gets stored , remember
+        this->x = x;  //they have different memory assigned for each class made
+        this->y = y;  
     }
-    base operator+(base &b){
-        base result;
-        result.x = b.x + x;
-        result.y = b.y + y;
-        return result;
-    }
-    
-    void print()
+    base operator+(base &b){//now comes base b, here we are doing b1+2
+        base result;        //which means b1.operator+(b2)
+        result.x = b.x + x; //this means b1 class is called and passing b2 into
+        result.y = b.y + y; //base &b so we can access b2 via b.
+        return result;      //now result is created (which has its own members all of it)
+    }                       //result x and y are empty(default constructor)
+                            //so we are storing x of b1 and of b2 via b.x and store it in
+    void print()            //result's x and y 
     {
         cout << "x : " << x << endl;
         cout << "y : " << y << endl;   
