bde5abd4440bb7a4fca0c557a2f8fe909b431a7b
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-01 23:45:02 +0500

    abtract and pure virtual done:)

--- abstract_pureVirtual.cpp
+++ abstract_pureVirtual.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 class base{
     protected:
-    int x;int y;
+    int x;
+    int y;
     public:
     base(int x, int y){
         this->x = x;
@@ -12,21 +13,44 @@ class base{
     virtual void showFunc() = 0;
 
 };
-class derived : public base{
+class derived1 : public base{
     protected:
-    int x,y;
     int z;
     public:
-    derived(int x,int y,int z):base(x,y){
-        this->x = x;
-        this->y = y;
+    derived1(int x,int y,int z):base(x,y){
         this->z = z;
+    }
+    void showFunc() override{
+        cout<<"derived1"<<endl;
+        cout<<x<<endl;
+        cout<<y<<endl;
+        cout<<z<<endl;
+    }
+};
+class derived2 : public base{
+    protected:
+    int z;
+    public:
+    derived2(int x,int y,int z):base(x,y){
+        this->z = z;
+    }
+    void showFunc() override{
+        cout<<"derived2"<<endl;
+        cout<<x<<endl;
+        cout<<y<<endl;
+        cout<<z<<endl;
     }
 };
 
 int main() {
+    base * ptr1 = new derived1(1,2,3);
+    ptr1->showFunc();
 
+    base * ptr2 = new derived2(4,5,6);
+    ptr2->showFunc();
 
-    
+    delete ptr1;
+    delete ptr2;
+
     return 0;
 }
