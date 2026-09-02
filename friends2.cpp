a16e2e0162ca3aa986c9c44097620655f21f41e7
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-02 20:11:38 +0500

    new file friends2

--- friends2.cpp
+++ friends2.cpp
@@ -13,16 +13,20 @@ public:
         this->cash = cash;
     }
     friend void transfer(Wallet &w, Bank &b, int amount);
-    friend void Print(const Wallet &w,const Bank &b);
+    friend void Print(const Wallet &w, const Bank &b);
 };
 class Bank
 {
-    int vault;
+    int vault = 0;
 
 public:
+    Bank(int vault)
+    {
+        this->vault = vault;
+    }
 
     friend void transfer(Wallet &w, Bank &b, int amount);
-    friend void Print(const Wallet &w,const Bank &b);
+    friend void Print(const Wallet &w, const Bank &b);
 };
 void transfer(Wallet &w, Bank &b, int amount)
 {
@@ -33,18 +37,19 @@ void transfer(Wallet &w, Bank &b, int amount)
     w.cash -= amount;
     b.vault += amount;
 }
-void Print(const Wallet &w,const Bank &b){
-    cout<<"cash : "<<w.cash<<endl;
-    cout<<"in vault"<<b.vault<<endl;
+void Print(const Wallet &w, const Bank &b)
+{
+    cout << "cash : " << w.cash << endl;
+    cout << "in vault : " << b.vault << endl;
 }
 int main()
 {
-    Wallet w(5000);
-    Bank b;
+    Wallet w(100);
+    Bank b(30);
     int amount;
-    cout<<"enter amount"<<endl;
-    cin>>amount;
-    transfer(w,b,amount);
-    Print(w,b);
+    cout << "enter amount" << endl;
+    cin >> amount;
+    transfer(w, b, amount);
+    Print(w, b);
     return 0;
 }
