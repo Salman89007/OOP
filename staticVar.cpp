4cbbde0261fa1eceaba47ff059be7a6191c10115
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-02 23:06:30 +0500

    changes made to static Member file

--- staticVar.cpp
+++ staticVar.cpp
@@ -2,12 +2,24 @@
 using namespace std;
 
 class Employee{
-    
-
+    static int id;
+    public:
+    Employee(){
+        id++;
+    }
+    void Print(){
+        cout<<id<<endl;
+    }
 };
-
+int Employee::id = 0;//by default value is 0, no garbage
 
 int main() {
+    Employee e1;
+    e1.Print();
+    Employee e2;
+    e2.Print();
+    Employee e3;
+    e3.Print();
     
     return 0;
 }
