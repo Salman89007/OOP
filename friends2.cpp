61175dd9ed6f09b2fb595fbd9c37d8be59977b6f
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-02 19:59:49 +0500

    new file friends2

--- friends2.cpp
+++ friends2.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+class Bank;
 
+class Wallet{
+    int cash;
+    public:
+    Wallet(int cash){
+        this->cash = cash;
+    }
+    friend void transfer(Wallet &w, Bank &b, int amount);
+
+};
+class Bank{
+    int vault;
+    public:
+    Bank(int vault){
+        this->vault = vault;
+    }
+    friend void transfer(Wallet &w, Bank &b, int amount);
+};
+void transfer(Wallet &w, Bank &b, int amount){
+
+}
 
 int main() {
     
