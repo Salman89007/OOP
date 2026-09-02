b205f11e9bc71ec24e2e357efc7d0128bc88dc1f
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-02 23:15:56 +0500

    static Function completed

--- staticFunc.cpp
+++ staticFunc.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class Employee{
+    static int id;
+    public:
+    Employee(){
+        id++;
+    }
+    void Print(){
+        cout<<id<<endl;
+    }
+    static void PrintID(){
+        cout<<"ID is : "<<id<<endl;
+    }
+};
+int Employee::id = 0;//by default value is 0, no garbage
+
 int main() {
+    Employee e1;
+    e1.Print();
+    Employee::PrintID;
+    Employee e2;
+    e2.Print();
+    Employee::PrintID;
+    Employee e3;
+    e3.Print();
+    Employee::PrintID;
     
     return 0;
 }
