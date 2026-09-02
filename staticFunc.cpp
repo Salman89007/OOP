762d4f1ec2703faac43c1cb6aa42245c81a4632b
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-02 23:18:02 +0500

    static Function perfected

--- staticFunc.cpp
+++ staticFunc.cpp
@@ -19,13 +19,13 @@ int Employee::id = 0;//by default value is 0, no garbage
 int main() {
     Employee e1;
     e1.Print();
-    Employee::PrintID;
+    Employee::PrintID();
     Employee e2;
     e2.Print();
-    Employee::PrintID;
+    Employee::PrintID();
     Employee e3;
     e3.Print();
-    Employee::PrintID;
+    Employee::PrintID();
     
     return 0;
 }
