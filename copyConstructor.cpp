76c52e485c3c84072928bec9d26efd5e8fb7109e
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-03 23:13:38 +0500

    copy constructor version 1.1

--- copyConstructor.cpp
+++ copyConstructor.cpp
@@ -7,20 +7,25 @@ class Base{
     Base(int val){
         x = new int(val);
     }
-    Base(const Base& b){
-        x = new int(*b.x);
+
+    Base(const Base& b){       //copy constructor
+        x = new int(*b.x);     //b.x is basically b1.x which is a memory address
+                               //* is for dereferencing meaning a value there
     }
+
     void setVal(int val){
-        
-
+        *x = val;
     }
-
-
+    void Print(){
+        cout<<"data is : "<<*x<<endl;
+    }
 };
 
 int main() {
     Base b1(10);
     Base b2 = b1;//same as Base b2(b1)
-    
+    b2.setVal(99);
+    b1.Print();
+    b2.Print();
     return 0;
 }
