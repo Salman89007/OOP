5ecfcc1f8ee6966c38fa7ef5a5646a67365076c6
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-04 01:33:07 +0500

    DMA array of pointers v2

--- dynamicallyCreating_arrayOFpointers.cpp
+++ dynamicallyCreating_arrayOFpointers.cpp
@@ -3,19 +3,30 @@ using namespace std;
 
 int main() {
     int **arr = new int*[5];
+    //here we have int* arr[5];
+    //this can store 5 ints
+    arr[0] = new int[5];
+    arr[1] = new int[5];
+    arr[2] = new int[5];
+    arr[3] = new int[5];
+    arr[4] = new int[5];
     for (int i = 0; i < 5; i++)
     {
-        *arr = new int[i];
+        for (int j = 0; j < 5; j++)
+        {
+            cin>>arr[i][j];
+        }
+        
     }
+
     for (int i = 0; i < 5; i++)
     {
-        cin>>*arr[i];
+        for (int j = 0; j < 5; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        
     }
-    for (int i = 0; i < 5; i++)
-    {
-        cout<<*arr[i]<<" ";
-    }
-    
-    
+
     return 0;
 }
