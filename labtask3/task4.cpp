95dc2b527111fd81baefc97eb3f6ac0c700cc6e4
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-05 12:46:16 +0500

    labtask3 task4 v2.5

--- labtask3/task4.cpp
+++ labtask3/task4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 void Fillarray(int *arr, int row, int col)
 {
     cout << "enter elements: " << endl;
@@ -10,6 +11,18 @@ void Fillarray(int *arr, int row, int col)
             cin >> arr[i * col + j];
         }
     }
+}
+int* addFlattened2D(int* arr1, int* arr2, int* arr3, int rows, int cols)
+{
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            arr3[i * cols + j] += arr1[i * cols + j] + arr2[i * cols + j];
+        }
+    }
+    return arr3;
 }
 void printFlattened2D(int *arr, int rows, int cols)
 {
@@ -30,10 +43,24 @@ int main()
     cin >> m;
     cout << "enter columns : " << endl;
     cin >> n;
-    int *arr = new int[m * n];
-    Fillarray(arr, m, n);
-    printFlattened2D(arr, m, n);
-    delete[] arr;
+
+    int *arr1 = new int[m * n];
+    int *arr2 = new int[m * n];
+    int *arr3 = new int[m * n];
+
+    Fillarray(arr1, m, n);
+    Fillarray(arr2, m, n);
+
+    int * sum = addFlattened2D(arr1, arr2, arr3, m, n);
+    cout<<"sum is "<<*sum;
+
+    printFlattened2D(arr1, m, n);
+    printFlattened2D(arr2, m, n);
+    printFlattened2D(arr3, m, n);
+
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
 
     return 0;
 }
