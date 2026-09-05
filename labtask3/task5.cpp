62d64ba2bc29e3b5bdcf4b886c747b1d9c228947
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-05 15:48:12 +0500

    labtask3 task5

--- labtask3/task5.cpp
+++ labtask3/task5.cpp
@@ -20,6 +20,7 @@ int** transposeMatrix(int** arr, int rows, int cols){
 }
 
 void Print(int** arr, int rows, int cols){
+    
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
