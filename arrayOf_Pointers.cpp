5ee11649090c4d35bb5ab8066d273eb48a0c4f9a
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-05 11:45:43 +0500

    array of pointers v1.01

--- arrayOf_Pointers.cpp
+++ arrayOf_Pointers.cpp
@@ -1,49 +1,50 @@
 #include <iostream>
 using namespace std;
-int** boldMatrixSum(int arr[][3], int rows, int cols){
+int *boldMatrixSum(int arr[][3], int rows, int cols)
+{
     int sum = 0;
     int sumDiagonal = 0;
     int sumAntiDiagonal = 0;
-    int *ptr[3] = {&sum,&sumDiagonal,&sumAntiDiagonal};
+
+    int *ptr = new int[3];
+
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
         {
-            sum+= arr[i][j];
+            cin >> arr[i][j];
         }
     }
+
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
         {
-            sumDiagonal+= arr[i][j];
+            sum += arr[i][j];
         }
     }
     for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < cols ; j++)
-        {
-            sumAntiDiagonal+= arr[i][j];
-        }
+        sumDiagonal += arr[i][i];
     }
+    for (int i = 0; i < rows; i++)
+    {
+        sumAntiDiagonal += arr[i][cols-1-i];
+    }
+    ptr[0] = sum;
+    ptr[1] = sumDiagonal;
+    ptr[2] = sumAntiDiagonal;
 
     return ptr;
 }
 
-int main() {
+int main()
+{
     int arr[3][3];
-    
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cin>>arr[i][j];
-        }
-        
-    }
-    int** sum = boldMatrixSum(arr,3,3);
-    cout<<"sum of all elements is : "<<*sum[0]<<endl;
-    cout<<"sum of diagonal elements is : "<<*sum[1]<<endl;
-    cout<<"sum of antidiagonal elements is : "<<*sum[2]<<endl;
+
+    int *sum = boldMatrixSum(arr, 3, 3);
+    cout << "sum of all elements is : " << sum[0] << endl;
+    cout << "sum of diagonal elements is : " << sum[1] << endl;
+    cout << "sum of antidiagonal elements is : " << sum[2] << endl;
     return 0;
 }
