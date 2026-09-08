cbbc814ccda1770416d802caac4265515fc2c689
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-08 11:54:39 +0500

    OOP theory Assignment 1 v1.1

--- OOP_Assignment_theory1.cpp
+++ OOP_Assignment_theory1.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    cout<<"enter N"<<endl;
-    cin>>n;
+    cout << "enter N" << endl;
+    cin >> n;
     char name[] = "salman";
-    int * number = new int[n*n];
-    int ** ptr = new int*[n];
+    int *number = new int[n];
+    int **ptr = new int *[n];
     for (int i = 0; i < n; i++)
     {
-        int num;
-        cin>>num;
-        ptr[i] = new int[number[num]];
+        int cols;
+        cin >> cols;
+        ptr[i] = new int[number[cols]];
     }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "enter for ptr[" << i << "]" << "[" << j << "]" <<endl;
+            cin >> ptr[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "ptr[" << i << "]" << "[" << j << "]" <<" "<<endl;
+            cout << ptr[i][j];
+        }
+        cout<<endl;
+    }
+
     delete[] number;
     for (int i = 0; i < n; i++)
     {
         delete[] ptr[number[i]];
     }
     delete[] ptr;
-    
+
     return 0;
 }
