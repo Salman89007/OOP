5132c86d4c6873db4ab4c85959b9e34ac501deaa
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-08 12:18:36 +0500

    project theory OOP v2

--- OOP_Assignment_theory1.cpp
+++ OOP_Assignment_theory1.cpp
@@ -1,45 +1,57 @@
 #include <iostream>
 using namespace std;
 
+struct Room{
+    int power;
+    bool cursedFlag;
+    bool sealedFlag;
+};
+struct Floor{
+    Room* rooms; 
+    int roomCount;
+};
+
 int main()
 {
     int n;
     cout << "enter N" << endl;
     cin >> n;
     char name[] = "salman";
+    
     int *number = new int[n];
     int **ptr = new int *[n];
     for (int i = 0; i < n; i++)
     {
-        int cols;
-        cin >> cols;
-        ptr[i] = new int[number[cols]];
+        number[i] = n-i;
+        ptr[i] = new int[number[i]];
     }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < number[i]; j++)
         {
-            cout << "enter for ptr[" << i << "]" << "[" << j << "]" <<endl;
+            cout << "enter for ptr[" << i << "]" << "[" << j << "]" << endl;
             cin >> ptr[i][j];
         }
     }
+    cout<<"NAME IS : "<<name<<endl;
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < number[i]; j++)
         {
-            cout << "ptr[" << i << "]" << "[" << j << "]" <<" "<<endl;
-            cout << ptr[i][j];
+            cout << "ptr[" << i << "]" << "[" << j << "]" <<" ";
+            cout << ptr[i][j]<<endl;
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    delete[] number;
     for (int i = 0; i < n; i++)
     {
-        delete[] ptr[number[i]];
+        delete[] ptr[i];
     }
+
     delete[] ptr;
+    delete[] number;
 
     return 0;
 }
