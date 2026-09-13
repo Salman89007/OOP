8fb921f3b4f164682331f92ab0309066cc3d3968
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-13 19:18:07 +0500

    some new codes of labtask4 v1.1

--- labtask4/task6.cpp
+++ labtask4/task6.cpp
@@ -7,36 +7,14 @@ int* mergeArrays(int *arr1, int size1, int *arr2, int size2){
 
 int main()
 {
-    int rows;
+    int rows,cols;
     cout << "ROWS : " << endl;
     cin >> rows;
 
-    int *cols = new int[rows];
+    cout << "COLS : " << endl;
+    cin >> cols;
 
-    int **arr = new int *[rows];
-    for (int i = 0; i < rows; i++)
-    {
-        cout << "enter cols for row" << i + 1 << " : " << endl;
-        cin >> cols[i];
-        arr[i] = new int[cols[i]];
-    }
-    // filling
-    cout << "enter elements : " << endl;
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols[i]; j++)
-        {
-            cout << "arr " << i + 1 << " " << j + 1 << endl;
-            cin >> arr[i][j];
-        }
-    }
-
-    // freeing
-    for (int i = 0; i < rows; i++)
-    {
-        delete[] arr[i];
-    }
-    delete[] arr;
+    
 
     return 0;
 }
