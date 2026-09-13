23ca9a719feb8dd17375292c7fa2a944cf81248c
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-13 21:56:12 +0500

    finale all tasks done of labtask4

--- labtask4/task6.cpp
+++ labtask4/task6.cpp
@@ -2,19 +2,47 @@
 using namespace std;
 
 int* mergeArrays(int *arr1, int size1, int *arr2, int size2){
-
+    int * arr = new int[size1+size2];
+    for (int i = 0; i < size1; i++)
+    {
+        arr[i] = arr1[i];
+    }
+    for (int i = size1; i < size1+size2; i++)
+    {
+        arr[i] = arr2[i - size1];
+    }
+    return arr;
 }
 
 int main()
 {
-    int rows,cols;
-    cout << "ROWS : " << endl;
-    cin >> rows;
+    int size1,size2;
+    cout<<"Enter size1 : "<<endl;
+    cin>>size1;
+    int *arr1 = new int[size1];
 
-    cout << "COLS : " << endl;
-    cin >> cols;
+    cout<<"Enter size2 : "<<endl;
+    cin>>size2;
+    int *arr2 = new int[size2];
 
-    
+    cout<<"enter elements of arr1 : "<<endl;
+    for (int i = 0; i < size1; i++)
+    {
+        cin>>arr1[i];
+    }
 
+    cout<<"enter elements of arr2 : "<<endl;
+    for (int i = 0; i < size2; i++)
+    {
+        cin>>arr2[i];
+    }
+    int * merge = mergeArrays(arr1,size1,arr2,size2);
+    for (int i = 0; i < size1+size2; i++)
+    {
+        cout<<merge[i]<<" ";
+    }
+    delete[] arr1;
+    delete[] arr2;
+    delete[] merge;
     return 0;
 }
