ec44da0dfc05b31b574cf0692939b1f4a85d0643
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-16 23:39:25 +0500

    3rd code done for sessional 1

--- practice_sessional_1/code3.cpp
+++ practice_sessional_1/code3.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 using namespace std;
 
+void Insert_Print(int **ptr,int rows,int *cols){
+
+    //inserting data
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols[i]; j++)
+        {
+            cin>>ptr[i][j];
+        }
+    }
+
+    //printing
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols[i]; j++)
+        {
+            cout<<ptr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
 int main() {
-    
+    int rows;
+    cout << "enter rows : ";
+    cin >> rows;
+
+    int arr[rows];
+    int ** ptr = new int*[rows];
+    //allocation
+    for (int i = 0; i < rows; i++)
+    {
+        cout<<"enter cols for row "<<i+1<<" : ";
+        cin>>arr[i];
+        ptr[i] = new int[arr[i]];
+    }
+
+    Insert_Print(ptr,rows,arr);
+
     return 0;
 }
