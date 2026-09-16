774514488143921dc021569f91514eee640081c7
Salman89007 <salmanmehmoodshah149@gmail.com>
2026-09-16 22:06:29 +0500

    code 1 sessional1

--- practice_sessional_1/code1.cpp
+++ practice_sessional_1/code1.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
+int *create2D(int rows, int cols)
+{
+    int *ptr = new int[rows * cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> ptr[i * cols + j];
+        }
+    }
+    return ptr;
+}
+void free2D(int *arr)
+{
+    delete[] arr;
+}
+
+int main()
+{
+
+    int rows, cols;
+    cout << "enter rows : ";
+    cin >> rows;
+
+    cout << "enter cols : ";
+    cin >> cols;
+
+    int *ptr = create2D(rows, cols);
+    free2D(ptr);
+
     return 0;
 }
